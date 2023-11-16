56c68acf7f005d4358f2d4a758ab680d6acaf544
senateseo <swdanielseo@gmail.com>
2023-11-15 21:13:25 -0800

    :sparkles: Initial implementation of CarouselSlide

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,11 +1,17 @@
-module.exports = { 
-  plugins: ['jest'],
-  extends: ['plugin:jest/recommended'],
-  parserOptions: {
-      ecmaVersion: 6,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
+  module.exports = { 
+    plugins: ['react'],
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    settings: {
+      react: {
+        version:'16.4.2'
+      }
     },
-};
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+  };
--- src/tests/.eslintrc.js
+++ src/tests/.eslintrc.js
@@ -1,16 +1,11 @@
-  module.exports = { 
-    plugins: ['react'],
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    settings: {
-      react: {
-        version:'16.4.2'
-      }
-    },
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
+module.exports = { 
+  plugins: ['jest'],
+  extends: ['plugin:jest/recommended'],
+  parserOptions: {
+      ecmaVersion: 6,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
       },
-  };
+    },
+};
